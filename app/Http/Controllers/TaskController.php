3fe4ba297ec7977a6e640938fce97a4046e09b6a
Ricardo <diiaz2910@gmail.com>
2023-08-06 17:36:53 -0500

    Commit with migrations done, task controller and Task model

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all(); //Retrieve all tasks from the database.
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+        ]);
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    public function show(Task $task)
+    {
+        response()->json($task);
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'comppleted' => 'boolean'
+        ]);
+
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null,204);
+    }
+}
